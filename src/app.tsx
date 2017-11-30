a25413cf6ef8702aa76fb10d2915baaba3f0c649
Houraisan Kaguya <sun87959012@hotmail.com>
2017-11-30 19:33:54 +0800

    update

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
-class A {
-    constructor() {
-        console.log('aaa');
+import ReactDOM from 'react-dom';
+// import './scss/index.scss';
+class App extends React.Component {
+    render() {
+        return <h1>hello kaguya</h1>;  
     }
 }
 
-const a = new A();
+ReactDOM.render(<App />, document.querySelectorAll('root'));
