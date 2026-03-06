6a590e30fc38028ffd8012b59e4aa9a29c63f12a
Sun Yuguang <sun87959012@hotmail.com>
2026-03-07 01:03:23 +0800

    deploy: publish static site build

--- src/ts/CONSTANTS.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  IMAGE_LIST: [
-    './backgrounds/weibo/a25ec037gy1gfwh1shji1j21hc0u0b29.webp',
-    './backgrounds/weibo/a25ec037gy1gfwi8m1qurj21hc0u01kx.webp',
-    './backgrounds/weibo/a25ec037gy1gfwi8tuvgzj21hc0u07wh.webp',
-    './backgrounds/weibo/a25ec037gy1gfwi8yk25hj21hc0u0tx2.webp',
-  ],
-};
