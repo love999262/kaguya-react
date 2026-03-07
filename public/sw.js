d32b686900967c9374613296b1b7bda43a968720
Sun Yuguang <sun87959012@hotmail.com>
2026-03-07 13:45:59 +0800

    feat: preload all background images into sw cache on first install

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,37 @@
 const CACHE_VERSION = 'kaguya-bg-cache-v1';
 const BG_PATH_SEGMENT = '/backgrounds/';
+const PRELOAD_IMAGES = [
+  'backgrounds/bg-1.svg',
+  'backgrounds/bg-2.svg',
+  'backgrounds/bg-3.svg',
+  'backgrounds/bg-4.svg',
+  'backgrounds/bg-5.svg',
+  'backgrounds/bg-6.svg',
+  'backgrounds/weibo/a25ec037gy1gfwh1shji1j21hc0u0b29.jpg',
+  'backgrounds/weibo/a25ec037gy1gfwi8m1qurj21hc0u01kx.jpg',
+  'backgrounds/weibo/a25ec037gy1gfwi8tuvgzj21hc0u07wh.jpg',
+  'backgrounds/weibo/a25ec037gy1gfwi8yk25hj21hc0u0tx2.jpg',
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(self.skipWaiting());
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_VERSION);
+    const cachedKeys = await cache.keys();
+
+    if (cachedKeys.length === 0) {
+      await Promise.allSettled(
+        PRELOAD_IMAGES.map(async (path) => {
+          const url = new URL(path, self.registration.scope).toString();
+          const response = await fetch(url, { cache: 'no-store' });
+          if (response.ok) {
+            await cache.put(url, response.clone());
+          }
+        })
+      );
+    }
+
+    await self.skipWaiting();
+  })());
 });
 
 self.addEventListener('activate', (event) => {
